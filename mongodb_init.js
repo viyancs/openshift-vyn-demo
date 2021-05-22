396d05b259f727a3d4a73180ce495270dd23d62f
viyancs <msofyancs@gmail.com>
2021-05-22 21:47:10 +0700

    adding script for initialize mongodb replica set

---
+++ mongodb_init.js
@@ -0,0 +1,7 @@
+rs.initiate();
+rs.add("mongo-1.mongo:27017");
+rs.add("mongo-2.mongo:27017");
+cfg = rs.conf();
+cfg.members[0].host = "mongo-0.mongo:27017";
+rs.reconfig(cfg, {force: true});
+sleep(5000);
