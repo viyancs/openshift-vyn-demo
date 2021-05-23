f691dbeb360ae17c2b752ee8f4c40c418aa5f2c2
viyancs <msofyancs@gmail.com>
2021-05-23 18:27:43 +0700

    adding pipeline openshift sample

---
+++ mongodb_test_db.js
@@ -0,0 +1,3 @@
+use langdb;
+db.languages.insert({"name" : "go", "codedetail" : { "usecase" : "system, web, server-side", "rank" : 16, "compiled" : true, "homepage" : "https://golang.org", "download" : "https://golang.org/dl/", "votes" : 0}});
+db.languages.find().pretty();
